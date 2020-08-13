8da838faa23777492293fbb61d68612c626fe480
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-13 18:50:15 +0200

    start mister cocktail

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,10 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients['drinks'].each do |i|
+  Ingredient.create(name: i["strIngredient1"])
+end
