415b0df8f35a2913e244c1fb712d9d7745df816d
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-14 16:45:57 +0200

    added photo upload fonction

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -23,10 +23,15 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
 
